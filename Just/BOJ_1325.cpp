0832f6581b2ca655011962a24859db847638b54f
TomorrowIppo <91578281+TomorrowIppo@users.noreply.github.com>
2024-02-05 14:40:08 +0000

    시작 노드에 따른 이동 가능한 노드의 개수 구하는 문제

--- Just/BOJ_1325.cpp
+++ Just/BOJ_1325.cpp
@@ -5,13 +5,8 @@
 
 using namespace std;
 
-bool cmp(pair<int, int> p1, pair<int, int> p2) 
-{
-    if(p1.Y == p2.Y) return p1.X < p2.X;
-    return p2.Y < p1.Y;
-}
 void input();
-void dfs(int v, int dist);
+void dfs(int v);
 void resetArr();
 
 int n, m;
@@ -30,20 +25,23 @@ int main()
     {
         resetArr();
         vis[i] = true;
-        dfs(i, 1);
+        dfs(i);
         ans.push_back({i, cnt});
         cnt = 0;
     }
-    // ans의 원소들을 비교해서 답 출력하는 부분 구현하기
-    sort(ans.begin(), ans.end(), cmp);
 
-    cout << ans[0].X << ' ';
-    for(int i = 1; i <= n; i++)
-    {
-        if(ans[i - 1].Y == ans[i].Y) { cout << ans[i].X << ' '; }
-        else break;
+    int maxHack = -1;
+    for (int i = 0; i < ans.size(); i++) {
+        if (ans[i].Y > maxHack) {
+            maxHack = ans[i].Y;
+        }
     }
-
+ 
+    for (int i = 0; i < ans.size(); i++) {
+        if (ans[i].Y == maxHack) {
+            cout << ans[i].X << " ";
+        }
+    }
     return 0;
 }
 
@@ -58,16 +56,16 @@ void input()
     }
 }
 
-void dfs(int v, int dist)
+void dfs(int v)
 {
     vis[v] = true;
     for(int nxt : graph[v])
     {
         if(vis[nxt]) continue;
         vis[nxt] = true;
-        dfs(nxt, dist + 1);
+        dfs(nxt);
+        cnt++;
     } 
-    cnt = max(cnt, dist);
 }
 
 void resetArr()
