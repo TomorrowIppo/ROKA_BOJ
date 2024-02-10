eb252fa4afcf79e90466eb609f3832fbdd0207e0
Kim Byeong Jin <91578281+TomorrowIppo@users.noreply.github.com>
2024-02-10 09:54:19 +0000

    Easy. start가 end보다 크거나 같을 경우 bfs를 안 돌림으로써 시간초과 방지

--- Just/BOJ_13913.cpp
+++ Just/BOJ_13913.cpp
@@ -14,7 +14,6 @@ void bfs(int start, int end);
 int n, k;
 int dx[2] = {-1, 1};
 bool vis[MAX];
-vector<int> root;
 
 int main()
 {
@@ -23,10 +22,17 @@ int main()
 
     // input
     cin >> n >> k;
-    root.push_back(n);
-    
+
     // solve
-    bfs(n, k);
+    if(n >= k)   // start가 end보다 크거나 같을 경우 bfs 할 필요 없음
+    {
+        cout << n - k << '\n';
+        cout << n << ' ';
+        for(int i = 1; i <= (n - k); i++)
+            cout << (n - i) << ' ';
+    }
+    else
+        bfs(n, k);
 
     return 0;
 }
@@ -34,5 +40,43 @@ int main()
 void bfs(int start, int end)
 {
     queue<dataSet> q;
-    q.push({start, vector<int>()});
+    vector<int> root = {start};
+    q.push({start, root});
+    vis[start] = true;
+
+    while(!q.empty())
+    {
+        auto info = q.front(); q.pop();
+
+        if(info.cur == end) 
+        {
+            cout << info.root.size() - 1 << '\n';
+            for(int i = 0; i < info.root.size(); i++)
+                cout << info.root[i] << ' ';
+            break;
+        }
+
+        // 좌 우 이동
+        for(int dir = 0; dir < 2; dir++)
+        {
+            int nx = info.cur + dx[dir];
+            vector<int> ls = info.root;
+
+            if(nx < 0 || nx >= MAX) continue;
+            if(vis[nx]) continue;
+
+            vis[nx] = true;
+            ls.push_back(nx);
+            q.push({nx, ls});
+        }
+
+        // 순간이동 처리
+        int nx = info.cur * 2;
+        if(nx >= MAX) continue;
+        if(vis[nx]) continue;
+
+        vis[nx] = true;
+        info.root.push_back(nx);
+        q.push({nx, info.root});
+    }
 }
