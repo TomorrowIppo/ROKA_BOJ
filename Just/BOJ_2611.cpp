82218e286efef344478acc9bb507a61db87f501d
Kim Byeong Jin <91578281+TomorrowIppo@users.noreply.github.com>
2024-04-03 14:03:13 +0000

    맛있네

--- Just/BOJ_2611.cpp
+++ Just/BOJ_2611.cpp
@@ -10,12 +10,17 @@ typedef struct
 }Info;
 
 void solve(int x);
+void topologicalSort();
+void dfs(int v);
 
 int n, m;
 int indegree[MAX];
 int dp[MAX];
 vector<Info> graph[MAX];
-int end_node;
+vector<Info> reverse_graph[MAX];
+vector<int> ans;
+bool vis[MAX];
+int end_node = 1;
 
 int main()
 {
@@ -28,6 +33,10 @@ int main()
         int p, q, r;
         cin >> p >> q >> r;
         graph[p].push_back({q, r});
+        reverse_graph[q].push_back({p, r});
+
+        if(q == 1) continue;
+
         indegree[q]++;
     }
     solve(1);
@@ -37,5 +46,52 @@ int main()
 
 void solve(int x)
 {
+    topologicalSort();
+    dfs(end_node);
+
+    ans.push_back(1);
+    reverse(ans.begin(), ans.end());
+    for(auto item : ans) cout << item << ' ';
+}
+
+void topologicalSort()
+{
+    queue<int> q;
+    for(int i = 1; i <= n; i++)
+        if(indegree[i] == 0) q.push(i);
     
+    while(!q.empty())
+    {
+        auto cur = q.front(); q.pop();
+
+        for(auto nxt : graph[cur])
+        {
+            int node = nxt.node;
+
+            dp[node] = max(dp[node], dp[cur] + nxt.r);
+
+            if(node == 1) continue;
+
+            indegree[node]--;
+            if(indegree[node] == 0) q.push(node);
+        }
+    }
+
+    for(int i = 2; i <= n; i++)
+        if(dp[end_node] < dp[i]) end_node = i;
+    
+    cout << dp[1] << '\n';
+}
+
+void dfs(int v)
+{
+    vis[v] = true;
+    ans.push_back(v);
+    for(auto nxt : reverse_graph[v])
+    {
+        if(vis[nxt.node]) continue;
+
+        if(dp[v] - nxt.r == dp[nxt.node])
+            dfs(nxt.node);
+    }
 }
