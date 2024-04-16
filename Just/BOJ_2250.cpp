4bcb8e5e76ddbefb14a986da0dd5819c5f3b858a
Kim Byeong Jin <91578281+TomorrowIppo@users.noreply.github.com>
2024-04-16 08:20:34 +0000

    난 왜 계속 쉬운 조건 하나를 못보고 원트클을 못하는 것인가

--- Just/BOJ_2250.cpp
+++ Just/BOJ_2250.cpp
@@ -1,14 +1,19 @@
 #include <bits/stdc++.h>
 #define MAX 10001
 
+/* 트리의 루트가 1이 아닐 수도 있어요! */
+
 using namespace std;
 
 int n;
 int tree[MAX][2];
 int cnt = 1;
 int pos[MAX];
+int parent[MAX];
 int max_depth = 0;
 vector<int> lv[MAX];
+
+bool cmp(int a, int b) { return pos[a] < pos[b]; }
 
 void inorder(int cur, int depth)
 {
@@ -29,11 +34,11 @@ pair<int, int> getMaxLength()
 
     for(int i = 1; i <= max_depth; i++)
     {
-        sort(lv[i].begin(), lv[i].end());
+        sort(lv[i].begin(), lv[i].end(), cmp);
 
-        int cmp = lv[i][lv[i].size() - 1] - lv[i][0] + 1;
-        cout << cmp << endl;
-        if(max_len < cmp) { max_lv = i; max_len = cmp; }
+        int tmp = pos[lv[i][lv[i].size() - 1]] - pos[lv[i][0]] + 1;
+        //cout << tmp << endl;
+        if(max_len < tmp) { max_lv = i; max_len = tmp; }
     }
 
     return {max_lv, max_len};
@@ -50,9 +55,14 @@ int main()
         int root, left, right;
         cin >> root >> left >> right;
         tree[root][0] = left; tree[root][1] = right;
+        parent[left] = root; parent[right] = root;
     }
 
-    inorder(1, 1);
+    int root = 0;
+    for(int i = 1; i <= n; i++)
+        if(parent[i] == 0) { root = i; break; }
+
+    inorder(root, 1);
     pair<int, int> res = getMaxLength();
     cout << res.first << ' ' << res.second;
 
