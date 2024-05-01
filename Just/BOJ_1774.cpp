a5dd56cc6afeb8f31c495b6cac50ef4bf086ad5e
Kim Byeong Jin <91578281+TomorrowIppo@users.noreply.github.com>
2024-05-01 14:59:08 +0000

    MST | 소수점 손실 방지하는 것이 엄청 까다로웠던 문제

--- Just/BOJ_1774.cpp
+++ Just/BOJ_1774.cpp
@@ -1,32 +1,38 @@
 #include <bits/stdc++.h>
 #define MAX 1001
 
-using namespace std;
-
 /*
 
-소수점 n번째 자리까지 반올림하여서 출력
-
-cout << fixed;
-cout.precision(n);
-cout << ans;
-
-형식으로 할 것
+역대급으로 조건이 까다로웠던 문제.
+getDist() 함수처럼 구현해서 소수점 손실을 방지하는 것이 관건이었다.
+이거에 2시간을 박은게 현타온다
 
 */
 
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
 typedef struct
 {
-    int x;
-    int y;
+    ll x;
+    ll y;
 }Coord;
 
 int n, m;
-double ans;
-vector<tuple<double, int, int>> edges;
+ld ans;
+vector<tuple<ld, int, int>> edges;
 vector<Coord> Coords;
 int parent[MAX];
 bool anti_duplication[MAX][MAX];
+
+ld getDist(Coord c1, Coord c2)
+{
+    ll dx = abs(c1.x - c2.x);
+    ll dy = abs(c1.y - c2.y);
+    return sqrt(pow(dx, 2) + pow(dy, 2));
+}
 
 int find_root(int x)
 {
@@ -34,17 +40,17 @@ int find_root(int x)
     return parent[x] = find_root(parent[x]);
 }
 
-bool is_Union(int x, int  y)
+bool is_Union(int x, int y)
 {
     x = find_root(x);
     y = find_root(y);
-
+    
     if(x == y) return true;
     else
     {
         if(x > y) parent[x] = y;
         else parent[y] = x;
-
+        
         return false;
     }
 }
@@ -53,68 +59,56 @@ int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-
+    
     // input
     cin >> n >> m;
     for(int i = 1; i <= n; i++) parent[i] = i;
-
+    
     for(int i = 1; i <= n; i++)
     {
-        int x, y;
+        ll x, y;
         cin >> x >> y;
         Coords.push_back({x, y});
     }
 
-    int duplication = 0;
+    int cnt = 0;
     for(int i = 0; i < m; i++)
     {
         int u, v;
         cin >> u >> v;
 
-        if(anti_duplication[u][v] || anti_duplication[v][u]) duplication++; // 중복 입력 방지 ex) 1 4과 4 1
+        if(anti_duplication[u][v] || anti_duplication[v][u]) cnt++; // 중복 입력 방지 ex) 1 4과 4 1
 
         anti_duplication[u][v] = true;
         anti_duplication[v][u] = true;
 
-        is_Union(u, v);   
+        //edges.push_back({0.0, u, v});
+        is_Union(u, v);
     }
 
-    // 이미 다 연결 되어있는 경우
-    if(n == m) { cout << "0.00"; return 0; }
-
-    // setting
     for(int i = 1; i < n; i++)
     {
         for(int j = i + 1; j <= n; j++)
         {
-            if(find_root(i) == find_root(j)) continue;
-            
-            int x1 = Coords[i - 1].x;
-            int y1 = Coords[i - 1].y;
-            int x2 = Coords[j - 1].x;
-            int y2 = Coords[j - 1].y;
-
-            double dist = sqrt(pow(x1 - x2, 2) + pow(y1 - y2, 2));
-            edges.push_back({dist, i , j});
+            ld dist = getDist(Coords[i - 1], Coords[j - 1]);
+            edges.push_back({dist, i, j});
         }
     }
     sort(edges.begin(), edges.end());
-
+    
     // solve
-    int cnt = 0;
     for(auto nxt : edges)
     {
-        double dist;
-        int u, v;
+        ld dist; int u, v;
         tie(dist, u, v) = nxt;
-
+        
         if(is_Union(u, v)) continue;
-
+        
         cnt++; ans += dist;
-
-        if(cnt == n - m - 1 + duplication) break;
+        if(cnt == n - 1) break;
     }
-    cout << ans;
-
+    cout.precision(2);
+    cout << fixed << ans;
+    
     return 0;
 }
