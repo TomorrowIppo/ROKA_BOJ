9ac7d453c6715ff432be387ea2b5e236cc4d632d
Kim Byeong Jin <91578281+TomorrowIppo@users.noreply.github.com>
2024-05-22 14:49:36 +0000

    애드혹 맛보기

---
+++ Just/BOJ_1402.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int a, b, t;
+    cin >> t;
+    while(t--)
+    {
+        cin >> a >> b;
+        cout << "yes\n";
+    }
+
+    return 0;
+}
