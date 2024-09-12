fafeb8d40373613ed9c9b132131823ee013e35b8
Kim Byeong Jin <91578281+TomorrowIppo@users.noreply.github.com>
2024-09-12 02:36:00 +0000

    이따 다시

--- Just/BOJ_1074.cpp
+++ Just/BOJ_1074.cpp
@@ -3,27 +3,53 @@
 using namespace std;
 
 int num = 0;
-vector<vector<int>> _map(MAX, vector<int>(MAX, 0));
+int r, c;
+int ans = 0;
 
 void func(int n, int x, int y)
 {
-    if(n > 1)
+    int range = pow(2, n);
+    int x_to = 0;
+    int y_to = 0;
+    int area = pow(range / 2, 2);
+
+    if(n == 1)
     {
-        int x_to = x + pow(2, n - 1);
-        int y_to = y + pow(2, n - 1);
-        
-        func(n - 1, x, y);
-        func(n - 1, x, y_to);
-        func(n - 1, x_to, y);
+        if(x == r && y == c) cout << ans;
+        if(x == r && y == c + 1) cout << ans + 1;
+        if(x == r + 1 && y == c) cout << ans + 2;
+        if(x == r + 1 && y == c + 1) cout << ans + 3;
+
+        return ;
+    }
+    else
+    {
+        if(0 <= r && r <= (range / 2) - 1 && 0 <= c && c <= (range / 2) - 1)    // 1번 영역
+        {
+            x_to = x; 
+            y_to = y;
+        }
+        if(0 <= r && r <= (range / 2) - 1 && (range / 2) <= c && c <= range - 1)    // 2번 영역
+        {
+            x_to = x; 
+            y_to = (range / 2) - 1;
+            ans = area;
+        }
+        if((range / 2) <= r && r <= range - 1 && 0 <= c && c <= (range / 2) - 1)    // 3번 영역
+        {
+            x_to = (range / 2) - 1; 
+            y_to = y;
+            ans = area * 2;
+        }
+        if((range / 2) <= r && r <= range - 1 && (range / 2) <= c && c <= range - 1)    // 4번 영역
+        {
+            x_to = (range / 2) - 1; 
+            y_to = (range / 2) - 1;
+            ans = area * 3;
+        }
         func(n - 1, x_to, y_to);
     }
-    else if(n == 1)
-    {
-        _map[x][y] = num++;
-        _map[x][y + 1] = num++;
-        _map[x + 1][y] = num++;
-        _map[x + 1][y + 1] = num++;
-    }
+
 }
 
 int main()
@@ -31,11 +57,10 @@ int main()
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    int n, r, c;
+    int n;
     cin >> n >> r >> c;
     
-    func(n, 0, 0);
-    cout << _map[r][c];
+    func(n, r, c);
     
     return 0;
 }
