5b97a24e7e6a2173238ccf9890dbf65a4b526344
Kim Byeong Jin <91578281+TomorrowIppo@users.noreply.github.com>
2024-09-12 04:23:21 +0000

    분할정복 | 다시 풀어볼만한 문제

--- Just/BOJ_1074.cpp
+++ Just/BOJ_1074.cpp
@@ -1,66 +1,32 @@
 #include <bits/stdc++.h>
-#define MAX 32769
+
 using namespace std;
 
-int num = 0;
-int r, c;
 int ans = 0;
+int n, r, c;
 
-void func(int n, int x, int y)
+void func(int x, int y, int size)   // 사분면 중 있는 r, c가 속한 사분면으로 계속 파고드는 구조
 {
-    int range = pow(2, n);
-    int x_to = 0;
-    int y_to = 0;
-    int area = pow(range / 2, 2);
-
-    if(n == 1)
+    if(x == r && y == c) { cout << ans; return ; }
+    else if(r < x + size && c < y + size && x <= r && y <= c)   // 분할정복
     {
-        if(x == r && y == c) cout << ans;
-        if(x == r && y == c + 1) cout << ans + 1;
-        if(x == r + 1 && y == c) cout << ans + 2;
-        if(x == r + 1 && y == c + 1) cout << ans + 3;
-
-        return ;
+        func(x, y, size / 2);
+        func(x, y + size / 2, size / 2);
+        func(x + size / 2, y, size / 2);
+        func(x + size / 2, y + size / 2, size / 2);
     }
-    else
-    {
-        if(0 <= r && r <= (range / 2) - 1 && 0 <= c && c <= (range / 2) - 1)    // 1번 영역
-        {
-            x_to = x; 
-            y_to = y;
-        }
-        if(0 <= r && r <= (range / 2) - 1 && (range / 2) <= c && c <= range - 1)    // 2번 영역
-        {
-            x_to = x; 
-            y_to = (range / 2) - 1;
-            ans = area;
-        }
-        if((range / 2) <= r && r <= range - 1 && 0 <= c && c <= (range / 2) - 1)    // 3번 영역
-        {
-            x_to = (range / 2) - 1; 
-            y_to = y;
-            ans = area * 2;
-        }
-        if((range / 2) <= r && r <= range - 1 && (range / 2) <= c && c <= range - 1)    // 4번 영역
-        {
-            x_to = (range / 2) - 1; 
-            y_to = (range / 2) - 1;
-            ans = area * 3;
-        }
-        func(n - 1, x_to, y_to);
-    }
+    else { ans += size * size; }    // 없으면 넓이 더하기
 
+    return ;
 }
 
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
-    int n;
+
     cin >> n >> r >> c;
-    
-    func(n, r, c);
-    
+    func(0, 0, pow(2, n));
+
     return 0;
 }
