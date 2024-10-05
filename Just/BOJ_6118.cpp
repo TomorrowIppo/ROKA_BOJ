2f1f04e2e8aaead30956850cd65b19ca3b143c43
Kim Byeong Jin <91578281+TomorrowIppo@users.noreply.github.com>
2024-10-05 03:41:01 +0000

    BFS 기초

--- Just/BOJ_6118.cpp
+++ Just/BOJ_6118.cpp
@@ -9,7 +9,8 @@ vector<int> graph[MAX];
 int dist[MAX];
 
 void solve();
-tuple<int, int, int> bfs(int x);
+void bfs(int x);
+tuple<int, int, int> get_ans();
 
 int main()
 {
@@ -34,20 +35,18 @@ void solve()
 
     for(int i = 1; i <= n; i++) dist[i] = -1;
     
+    bfs(1);
+
     int num, max_dist, cnt;
-    tie(num, max_dist, cnt) = bfs(1);
+    tie(num, max_dist, cnt) = get_ans();
 
     cout << num << ' ' << max_dist << ' ' << cnt << endl;
 }
 
-tuple<int, int, int> bfs(int x)
+void bfs(int x)
 {
     queue<int> q; q.push(x);
     dist[x] = 0;
-
-    int num = 0;
-    int max_dist = 0;
-    int cnt = 0;
 
     while(!q.empty())
     {
@@ -56,6 +55,31 @@ tuple<int, int, int> bfs(int x)
         {
             if(dist[nxt] >= 0) continue;    // 재방문 방지
             
+            q.push(nxt);
+            dist[nxt] = dist[cur] + 1;   
         }
     }
+
+    // for(int i = 1; i <= n; i++)
+    //     cout << dist[i] << endl;
+}
+
+tuple<int, int, int> get_ans()
+{
+    int num = 0;
+    int max_dist = 0;
+    int cnt = 0;
+
+    for(int i = 1; i <= n; i++)
+    {
+        if(max_dist < dist[i])
+        {
+            max_dist = dist[i];
+            cnt = 1;
+            num = i;
+        }
+        else if(max_dist == dist[i]) cnt++;
+    }
+
+    return {num, max_dist, cnt};
 }
